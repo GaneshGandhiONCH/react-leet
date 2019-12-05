0688a20493516ff31907917bbfecdd473957a8b3
Megan McGee <meganpmcgee@gmail.com>
2019-12-05 15:32:48 -0800

    Remove boilerplate code. Create components file and directories for each excercise. Create Challenges list component.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import Container from 'react-bootstrap/Container';
+import ChallengeTable from './components/ChallengeList/index.js';
+import Row from 'react-bootstrap/Row';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="mt-5 ml-5">
+        <h1>Leet Code Excercises built in React</h1> 
+        <p>This is the implementation of Leet Code excercises in React for the sake of practicing algorithm construction while using tools of the trade. The goal of this is to demonstrate the React way of approaching these problems.</p>
       </header>
+      {/* Table will list all excercises and link to corresponding components */}
+        <Container className="ml-5"> 
+          <Row>
+            <code>Challenges will go here. </code>
+          </Row>
+        </Container>
     </div>
   );
 }
